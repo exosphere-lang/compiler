9ebd5f17444ded573874dd24ba6dcf38ee2f7f58
chris-bacon <chrisbacon2009@hotmail.com>
2019-01-16 12:38:57 +0000

    Added more tests for Parsing logic
    Co-authored-by: Chris Bacon <chrisbacon2009@hotmail.com>
    Co-authored-by: Jacob Clark <jacob.jh.clark@googlemail.com>

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,13 +1,16 @@
 module Parser where
 
 import qualified AST as AST
-import CFError
+import ParseErrors
 import Grammar
 
-parse :: Program -> [Either CFError AST.Resource]
-parse p = map createResourceCFTree $ map getTokens $ getResources $ p 
+parse :: Program -> [Either ParseErrors AST.Resource]
+parse (Program []) = [Left EmptyProgram]
+parse p = map createResourceCFTree $ map getTokens $ getResources $ p
 
-createResourceCFTree :: [Token] -> Either CFError AST.Resource
+createResourceCFTree :: [Token] -> Either ParseErrors AST.Resource
+createResourceCFTree [] = Left $ EmptyProgram
 createResourceCFTree (Word resourceName:Keyword serviceType:_) =  Right $ AST.Resource resourceName serviceType
-createResourceCFTree (Word resourceName:[]) = Left $ InvalidResource "No resource type specified"
-createResourceCFTree (Keyword serviceType:Word resourceName:_) = Left $ InvalidResource "Resource type should come after resource name"
+createResourceCFTree (Word _:[]) = Left $ InvalidResource "No resource type specified"
+createResourceCFTree (Keyword _:_) = Left $ InvalidResource "Resource type should come after resource name"
+createResourceCFTree (Word _: Word _:_) = Left $ InvalidResource "A resource type must be specified following a resource name"
