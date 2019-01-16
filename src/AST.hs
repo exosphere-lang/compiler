246cd16a00cbd6670da58547f85f946f76042690
chris-bacon <chrisbacon2009@hotmail.com>
2019-01-16 20:20:21 +0000

    deleted unused AST

--- src/AST.hs
+++
@@ -1,8 +0,0 @@
-module AST where
-
-import ServiceType (ServiceType)
-
-data AST      = AST [Resource] deriving (Eq, Show)
-data Resource = Resource ResourceName ServiceType deriving (Eq, Show)
-
-type ResourceName = String
