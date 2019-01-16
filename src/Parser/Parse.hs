4ea40795f6bca0fe3001f5de8b45a06ce9f33190
chris-bacon <chrisbacon2009@hotmail.com>
2019-01-16 20:20:27 +0000

    Added type sig

--- src/Parser/Parse.hs
+++ src/Parser/Parse.hs
@@ -1,5 +1,9 @@
 module Parser.Parse where
 
+import Lexer.Grammar
 import Parser.SyntacticAnalysis (analyse)
+import Parser.AST
+import Parser.ParseError.Errors
 
+parse :: Program -> Either [ParseError] AST
 parse = analyse
