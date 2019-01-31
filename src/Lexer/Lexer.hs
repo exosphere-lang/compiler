58f0f2523cea4c231412a28e92d8ad8685fac688
Chris Bacon <chrisbacon2009@hotmail.com>
2019-01-31 23:31:02 +0000

    Update Lexer.hs

--- src/Lexer/Lexer.hs
+++ src/Lexer/Lexer.hs
@@ -2,21 +2,19 @@
 
 module Lexer.Lexer where
 
-import qualified Data.Map.Strict as Map
+import qualified Data.Map.Strict          as Map
 import           Data.Set
-import qualified Lexer.Grammar as Grammar
+import qualified Lexer.Grammar            as Grammar
 import           Lexer.Keywords
 import qualified Parser.ParseError.Errors as PE
-import           Prelude hiding (lookup)
+import           Prelude                  hiding (lookup)
 import           Text.Megaparsec
 import           Text.Megaparsec.Char
 
-commentsSymbol :: String
-commentsSymbol = "//"
+type Parser      = Parsec PE.ParseError String
+type CustomError = ParseError (Token String) PE.ParseError
 
-type Parser = Parsec PE.ParseError String
-
-lexe :: String -> Either (ParseError (Token String) PE.ParseError) Grammar.Program
+lexe :: String -> Either CustomError Grammar.Program
 lexe programInput = parse parser "" programInput
 
 parser :: Parser Grammar.Program 
@@ -32,5 +30,5 @@ parser = do
 resourceTypeParser :: String -> Parser Grammar.Token
 resourceTypeParser resourceType = do
   case Map.lookup resourceType keywords of 
-    Nothing -> fancyFailure $ singleton $ ErrorCustom PE.NoResourceTypeSpecified
+    Nothing          -> fancyFailure $ singleton $ ErrorCustom PE.NoResourceTypeSpecified
     Just serviceType -> return serviceType
