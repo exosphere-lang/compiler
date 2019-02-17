a4195bf82595abfb09858c52c3c24734326beea7
Chris Bacon <chrisbacon2009@hotmail.com>
2019-02-17 18:17:26 +0000

    Provide Show instance for all Error types

--- src/Parser/AST.hs
+++ src/Parser/AST.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveGeneric     #-}
 
 module Parser.AST where
 
--- src/Parser/ParseError/Errors.hs
+++ src/Parser/ParseError/Errors.hs
@@ -4,9 +4,9 @@ import Text.Megaparsec.Error hiding (ParseError)
 
 data ParseError 
   = EmptyProgram
-  | NoResourceTypeSpecified
-  | ResourceTypeShouldComeAfterResourceName
-  | ResourceNameShouldComeBeforeResourceType
+  | NoServiceTypeSpecified
+  | ServiceTypeShouldComeAfterResourceName
+  | ResourceNameShouldComeBeforeServiceType
   | InvalidServiceTypeSpecified
   | FatalError
   deriving (Eq, Ord)
@@ -18,4 +18,9 @@ cloudFormationResourceTypeURL :: String
 cloudFormationResourceTypeURL = "https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-template-resource-type-ref.html"
 
 instance Show ParseError where
-  show InvalidServiceTypeSpecified = "Must be a valid CloudFormation resource type\n" ++ cloudFormationResourceTypeURL
+  show EmptyProgram                            = "You have supplied an empty program"
+  show NoServiceTypeSpecified                  = "Must supply an AWS service type"
+  show ServiceTypeShouldComeAfterResourceName  = "The service type must come after the resource name"
+  show ResourceNameShouldComeBeforeServiceType = "The service type must come after the resource name"
+  show InvalidServiceTypeSpecified             = "Must be a valid CloudFormation resource type\n" ++ cloudFormationResourceTypeURL
+  show FatalError                              = "Fatal error"
