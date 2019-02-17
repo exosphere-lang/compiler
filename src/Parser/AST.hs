750456999d543a418f6f27426a05c30e8c153fbc
Chris Bacon <chrisbacon2009@hotmail.com>
2019-02-17 18:24:25 +0000

    formatting

--- src/Parser/AST.hs
+++ src/Parser/AST.hs
@@ -8,12 +8,13 @@ import qualified Data.Text as T
 import           GHC.Generics
 import           ServiceType (ServiceType)
 
-data AST      = AST [Resource] deriving (Eq, Show, Generic)
-data Resource = Resource ResourceName ServiceType deriving (Eq, Show, Generic)
+data AST          = AST [Resource]                    deriving (Eq, Show, Generic)
+data Resource     = Resource ResourceName ServiceType deriving (Eq, Show, Generic)
+type ResourceName = String
 
 instance ToJSON AST
 
 instance ToJSON Resource where
-  toJSON (Resource resourceName serviceType) = object [(T.pack resourceName) .= object [ "Type" .= (show serviceType) ]]
-
-type ResourceName = String
+  toJSON (Resource resourceName serviceType) = object 
+    [ (T.pack resourceName) .= object [ "Type" .= (show serviceType) ]
+    ]
